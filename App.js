4e20943345a86489852b47ae157ba09de448cfb2
Huhroo <roope.v.huhta@gmail.com>
2024-02-18 17:00:31 +0200

    init

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Position from './components/Position.js';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.heading}>Current weather!</Text>
+      <Position/>
     </View>
   );
 }
@@ -13,8 +15,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#E1F0F4',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  heading:{
+    fontSize: 20,
+  }
 });
